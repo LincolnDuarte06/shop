312ccc62fd49b9cbf114639497357219a3e3510b
LincolnDuarte06 <lincolnduartebel@gmail.com>
2024-05-17 16:17:40 -0400

    17/05

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,20 @@
-
+import Image from "next/image";
+import style from "./page.module.css";
 export default async function Home() {
+    const response = await fetch("https://fakestoreapi.com/products/");
+    const data = await response.json();
 
   return (
-    <main>
+    <main className={style.main}>
+      {data.map((products)=> 
+      <div className={style.card} key={products.id}>
+        <p>{products.title}</p>
+        <Image
+        width={100}
+        height={100}
+        src={products.image}/>
+        </div>
+      )}
     </main>
   );
 }
